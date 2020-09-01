12e76fcfb40ec7482d1ba0e003c68f541c05f5f7
Afnan shawawreh <64199018+AfnanShawawreh@users.noreply.github.com>
2020-09-01 10:58:39 +0300

    “Done”

---
+++ src/components/projects-timeline/projects-timeline.component.jsx
@@ -0,0 +1,355 @@
+import React from "react";
+import { Timeline, Events, UrlButton, ImageEvent } from "@merc/react-timeline";
+
+// projects
+import L_book from "../../assets/img/projects/book.webp";
+import L_nany from "../../assets/img/projects/nany.webp";
+
+import L_Moody from "../../assets/img/projects/Moody.webp";
+import Accordion from "react-bootstrap/Accordion";
+import Card from "react-bootstrap/Card";
+
+// skills
+import L_REACT from "../../assets/img/skills/react.svg";
+import L_NODE_JS from "../../assets/img/skills/nodejs.svg";
+import L_EXPRESS from "../../assets/img/skills/express.svg";
+import L_reactnative from "../../assets/img/skills/reactnative.svg";
+
+import Image from "react-bootstrap/Image";
+import L_HEROKU from "../../assets/img/skills/heroku.svg";
+
+import L_MATERIALUI from "../../assets/img/skills/material-ui-1.svg";
+
+import "./projects-timeline.styles.css";
+
+const TimeLine = () => {
+  return (
+    <div id="projects">
+      <h1 className="pt-3 text-center font-details-b pb-3">PROJECTS</h1>
+      <Timeline>
+        <Events>
+          {/* Project:Nany app */}
+          <ImageEvent
+            className="text-center"
+            text="Nany App"
+            src={L_nany}
+            alt="Nany App"
+          >
+            <div className="d-flex justify-content-between flex-column mt-1">
+              <div>
+                <Accordion>
+                  <Card>
+                    <Accordion.Toggle
+                      as={Card.Header}
+                      eventKey="0"
+                      className="p-2 text-center accordian-main"
+                    >
+                      PROJECT DETAILS
+                    </Accordion.Toggle>
+
+                    <Accordion.Collapse eventKey="0" className="text-left">
+                      <Card.Body>
+                        <strong>Description:</strong> An Mobile App that help
+                        mothers to reserve baby sitter online
+                        <br />
+                        <strong>Tech used:</strong>
+                        <ul>
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_REACT}
+                                alt="React"
+                                rounded
+                                className="image-style1 m-1"
+                              ></Image>{" "}
+                              React
+                            </span>
+                            <li>
+                              <span className="p-2">
+                                <Image
+                                  src={L_reactnative}
+                                  alt="React Native"
+                                  rounded
+                                  className="image-style1 m-1"
+                                ></Image>{" "}
+                                React Native
+                              </span>
+                            </li>
+                            <li>
+                              <span className="p-2">
+                                <Image
+                                  src={L_NODE_JS}
+                                  alt="Node.js"
+                                  rounded
+                                  className="image-style1 m-1"
+                                ></Image>{" "}
+                                Node.js
+                              </span>
+                            </li>
+                            <li>
+                              <span className="p-2">
+                                <Image
+                                  src={L_EXPRESS}
+                                  alt="Express"
+                                  rounded
+                                  className="image-style1 m-1"
+                                ></Image>{" "}
+                                Express
+                              </span>
+                            </li>
+                            <li>
+                              <span className="p-2">
+                                <Image
+                                  src={L_MATERIALUI}
+                                  alt="material-Ui"
+                                  rounded
+                                  className="image-style1 m-1"
+                                ></Image>{" "}
+                                MATERIALUI
+                              </span>
+                            </li>
+                            <li>
+                              <span className="p-2">
+                                <Image
+                                  src={L_HEROKU}
+                                  alt="HEROKU"
+                                  rounded
+                                  className="image-style1 m-1"
+                                ></Image>{" "}
+                                HEROKU
+                              </span>
+                            </li>
+                          </li>
+                        </ul>
+                      </Card.Body>
+                    </Accordion.Collapse>
+                  </Card>
+                </Accordion>
+              </div>
+              <div className="d-flex justify-content-between flex-nowrap text-center">
+                <UrlButton
+                  href="https://github.com/Nany-Cares-Thesis-Project"
+                  target="_blank"
+                >
+                  SOURCE CODE
+                </UrlButton>
+                <UrlButton
+                  href="https://www.youtube.com/watch?v=380EWQJzTAE"
+                  target="_blank"
+                >
+                  WATCH VIDEO
+                </UrlButton>
+              </div>
+            </div>
+          </ImageEvent>
+
+          {/* Project:BOOK Teacher */}
+          <ImageEvent
+            className="text-center"
+            text="Book Teacher"
+            src={L_book}
+            alt="Book Teacher"
+          >
+            <div className="d-flex justify-content-between flex-column mt-1">
+              <div>
+                <Accordion>
+                  <Card>
+                    <Accordion.Toggle
+                      as={Card.Header}
+                      eventKey="0"
+                      className="p-2 text-center accordian-main"
+                    >
+                      PROJECT DETAILS
+                    </Accordion.Toggle>
+
+                    <Accordion.Collapse eventKey="0" className="text-left">
+                      <Card.Body>
+                        <strong>Description:</strong> Web App allows
+                        communication between teachers and students who need
+                        study help.
+                        <br />
+                        <strong>Tech used:</strong>
+                        <ul>
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_REACT}
+                                alt="React"
+                                rounded
+                                className="image-style1 m-1"
+                              ></Image>{" "}
+                              React
+                            </span>
+
+                            <li>
+                              <span className="p-2">
+                                <Image
+                                  src={L_NODE_JS}
+                                  alt="Node.js"
+                                  rounded
+                                  className="image-style1 m-1"
+                                ></Image>{" "}
+                                Node.js
+                              </span>
+                            </li>
+                            <li>
+                              <span className="p-2">
+                                <Image
+                                  src={L_EXPRESS}
+                                  alt="Express"
+                                  rounded
+                                  className="image-style1 m-1"
+                                ></Image>{" "}
+                                Express
+                              </span>
+                            </li>
+                            <li>
+                              <span className="p-2">
+                                <Image
+                                  src={L_MATERIALUI}
+                                  alt="material-Ui"
+                                  rounded
+                                  className="image-style1 m-1"
+                                ></Image>{" "}
+                                MATERIALUI
+                              </span>
+                            </li>
+                            <li>
+                              <span className="p-2">
+                                <Image
+                                  src={L_HEROKU}
+                                  alt="HEROKU"
+                                  rounded
+                                  className="image-style1 m-1"
+                                ></Image>{" "}
+                                HEROKU
+                              </span>
+                            </li>
+                          </li>
+                        </ul>
+                        <hr />
+                      </Card.Body>
+                    </Accordion.Collapse>
+                  </Card>
+                </Accordion>
+              </div>
+              <div className="d-flex justify-content-between flex-nowrap text-center">
+                <UrlButton
+                  href="https://github.com/Team-Six-GreenField"
+                  target="_blank"
+                >
+                  SOURCE CODE
+                </UrlButton>
+                <UrlButton href="https://youtu.be/pMADUGiT2Pg" target="_blank">
+                  WATCH VIDEO
+                </UrlButton>
+              </div>
+            </div>
+          </ImageEvent>
+
+          {/* Project:Moody App */}
+          <ImageEvent
+            className="text-center"
+            text="Mody APP"
+            src={L_Moody}
+            alt="Mody APP"
+          >
+            <div className="d-flex justify-content-between flex-column mt-1">
+              <div>
+                <Accordion>
+                  <Card>
+                    <Accordion.Toggle
+                      as={Card.Header}
+                      eventKey="0"
+                      className="p-2 text-center accordian-main"
+                    >
+                      PROJECT DETAILS
+                    </Accordion.Toggle>
+
+                    <Accordion.Collapse eventKey="0" className="text-left">
+                      <Card.Body>
+                        <strong>Description:</strong>Web App allows user to
+                        listen to song depending on his mood
+                        <br />
+                        <strong>Tech used:</strong>
+                        <ul>
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_REACT}
+                                alt="React"
+                                rounded
+                                className="image-style1 m-1"
+                              ></Image>{" "}
+                              React
+                            </span>
+
+                            <li>
+                              <span className="p-2">
+                                <Image
+                                  src={L_NODE_JS}
+                                  alt="Node.js"
+                                  rounded
+                                  className="image-style1 m-1"
+                                ></Image>{" "}
+                                Node.js
+                              </span>
+                            </li>
+                            <li>
+                              <span className="p-2">
+                                <Image
+                                  src={L_EXPRESS}
+                                  alt="Express"
+                                  rounded
+                                  className="image-style1 m-1"
+                                ></Image>{" "}
+                                Express
+                              </span>
+                            </li>
+                            <li>
+                              <span className="p-2">
+                                <Image
+                                  src={L_MATERIALUI}
+                                  alt="material-Ui"
+                                  rounded
+                                  className="image-style1 m-1"
+                                ></Image>{" "}
+                                MATERIALUI
+                              </span>
+                            </li>
+                            <li>
+                              <span className="p-2">
+                                <Image
+                                  src={L_HEROKU}
+                                  alt="HEROKU"
+                                  rounded
+                                  className="image-style1 m-1"
+                                ></Image>{" "}
+                                HEROKU
+                              </span>
+                            </li>
+                          </li>
+                        </ul>
+                        <hr />
+                      </Card.Body>
+                    </Accordion.Collapse>
+                  </Card>
+                </Accordion>
+              </div>
+              <div className="d-flex justify-content-between flex-nowrap text-center">
+                <UrlButton
+                  href="https://github.com/The-Moody-App"
+                  target="_blank"
+                >
+                  SOURCE CODE
+                </UrlButton>
+              </div>
+            </div>
+          </ImageEvent>
+        </Events>
+      </Timeline>
+    </div>
+  );
+};
+
+export default TimeLine;
---
+++ src/pages/about/about.component.jsx
@@ -0,0 +1,105 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "./about.styles.css";
+import Profile from "../../assets/img/profile/profile.webp";
+import Image from "react-bootstrap/Image";
+import Button from "react-bootstrap/Button";
+
+const About = () => {
+  return (
+    <div id="about">
+      <div className="about">
+        <h1 className="pt-3 text-center font-details pb-3">ABOUT ME</h1>
+        <Container>
+          <Row className="pt-3 pb-5 align-items-center">
+            <Col xs={12} md={6}>
+              <Row className="justify-content-center mb-2 mr-2 ">
+              Hi there! I am <strong>&nbsp;Afnan Shawawreh</strong>
+                <br /> A creative, curious, motivated software developer who is
+                always
+                 eager to learn new technologies and constantly develop
+                and
+                <br /> improve, at the same time apply what I know and add value
+                 wherever I go.
+                 <br />  Graduated from Mutah University in 2017
+                with a
+                bachelor's degree in Telecommunication Engineering, I
+                trained at
+                <br />  Arab Potash Company for one year and 5 months as a
+                helpdesk Engineer in this period I took a lot of courses like
+                CCNA,
+                <br />  PV System design, PLC , then joined SHETECHS three months
+                special program initiated by int@j, Which is concerned with
+                empowering women in the information technology sector , 
+                <br /> then I joined RebootKamp (Rbk) An intensive 20-week program, Full Stack
+                Web Based on Hack Reactor curriculum.
+                <br /> <br />
+                <Col className="d-flex justify-content-center flex-wrap">
+                  <div>
+                    <a href="#contact">
+                      <Button className="m-2" variant="outline-primary">
+                        Let's talk
+                      </Button>
+                    </a>
+                  </div>
+                  <div>
+                    <a
+                      href="https://drive.google.com/file/d/1kyZ6wte3IFKS0dU_n-pvGvTqz831M4Me/view?usp=drivesdk"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Button className="m-2" variant="outline-success">
+                        My Resume
+                      </Button>
+                    </a>
+                  </div>
+                  <div>
+                    <a
+                      href="https://github.com/AfnanShawawreh"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Button className="m-2" variant="outline-dark">
+                        GitHub
+                      </Button>
+                    </a>
+                  </div>
+                  <div>
+                    <a
+                      href="https://www.linkedin.com/in/afnan-shawawreh-74b8b51a1/ "
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Button className="m-2" variant="outline-info">
+                        LinkedIn
+                      </Button>
+                    </a>
+                  </div>
+                </Col>
+              </Row>
+            </Col>
+            <Col xs={12} md={6}>
+              <Row className=" align-items-start p-2 my-details rounded">
+              <Image
+                  className="profile justify-content-end"
+                  alt="profile"
+                  src={Profile}
+                  thumbnail
+                  fluid
+                />
+               
+
+
+                
+              </Row>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </div>
+  );
+};
+
+export default About;
