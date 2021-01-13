5ae17e61c642b030bb81d27666dc8291e00d75d1
Afnan shawawreh <64199018+AfnanShawawreh@users.noreply.github.com>
2021-01-13 12:53:31 +0200

    Updates

---
+++ precache-manifest.95692ec3f17d8bcb9bc7fb8ec0882a66.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8c840ce5a025385cad2f802db10454f",
+    "url": "/Myportfolio/index.html"
+  },
+  {
+    "revision": "69a00e267542dfb8df36",
+    "url": "/Myportfolio/static/css/main.9f50d541.chunk.css"
+  },
+  {
+    "revision": "0db0a45edd0332358d1d",
+    "url": "/Myportfolio/static/js/2.2eb4a02e.chunk.js"
+  },
+  {
+    "revision": "176c1a1a28b19c611d9701905d8d60b7",
+    "url": "/Myportfolio/static/js/2.2eb4a02e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69a00e267542dfb8df36",
+    "url": "/Myportfolio/static/js/main.bc7fbb40.chunk.js"
+  },
+  {
+    "revision": "5caf23caf7b909f20585",
+    "url": "/Myportfolio/static/js/runtime-main.8164b399.js"
+  },
+  {
+    "revision": "a06a3e66aaa822df00f43463a815ce5f",
+    "url": "/Myportfolio/static/media/Moody.a06a3e66.webp"
+  },
+  {
+    "revision": "4912f6d800e1b75354e0b5d4794e1856",
+    "url": "/Myportfolio/static/media/Redux.4912f6d8.png"
+  },
+  {
+    "revision": "bf989778565c49da91f9e0ef13dbdc5c",
+    "url": "/Myportfolio/static/media/background.bf989778.webp"
+  },
+  {
+    "revision": "e95d05630894e5403e569fdaa0a2b61a",
+    "url": "/Myportfolio/static/media/background.e95d0563.webp"
+  },
+  {
+    "revision": "7dcd01fb4e171bd75efde879924b0182",
+    "url": "/Myportfolio/static/media/book.7dcd01fb.webp"
+  },
+  {
+    "revision": "ffded2381d25dd906406d6e07f91dea5",
+    "url": "/Myportfolio/static/media/bootstrap-4.ffded238.svg"
+  },
+  {
+    "revision": "3707da3c6a394c7ee2523273f8d98a25",
+    "url": "/Myportfolio/static/media/css3.3707da3c.svg"
+  },
+  {
+    "revision": "c6bab64b183f96aa47077fd3db37afd0",
+    "url": "/Myportfolio/static/media/express.c6bab64b.svg"
+  },
+  {
+    "revision": "1190a19ce3cc940741a60137a1e074cb",
+    "url": "/Myportfolio/static/media/git-icon.1190a19c.svg"
+  },
+  {
+    "revision": "0ff1a6f08182be4aa63a0b54da1b9e57",
+    "url": "/Myportfolio/static/media/github.0ff1a6f0.svg"
+  },
+  {
+    "revision": "b38ff61aa327fbdc24f7b1dad7d04d1a",
+    "url": "/Myportfolio/static/media/heroku.b38ff61a.svg"
+  },
+  {
+    "revision": "676e8188f003ed5d4882a0d0f378dad3",
+    "url": "/Myportfolio/static/media/html-5.676e8188.svg"
+  },
+  {
+    "revision": "2d2e31cc63a96c7e6044f52d30fd21b9",
+    "url": "/Myportfolio/static/media/javascript.2d2e31cc.svg"
+  },
+  {
+    "revision": "4dbbe08404523021f7393f21deeaef89",
+    "url": "/Myportfolio/static/media/logow.4dbbe084.webp"
+  },
+  {
+    "revision": "82291019f34ab54a677c456a84127cc3",
+    "url": "/Myportfolio/static/media/material-ui-1.82291019.svg"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/Myportfolio/static/media/memories.5c0c63fb.png"
+  },
+  {
+    "revision": "130f8aa7935fe42c898d07a061df1019",
+    "url": "/Myportfolio/static/media/mongodb.130f8aa7.svg"
+  },
+  {
+    "revision": "a0c60de292649537735153959ae2a776",
+    "url": "/Myportfolio/static/media/nany.a0c60de2.webp"
+  },
+  {
+    "revision": "a123152868245dee71c441124a98ac3e",
+    "url": "/Myportfolio/static/media/nodejs.a1231528.svg"
+  },
+  {
+    "revision": "be9b53854b592a33c39141c7605f835d",
+    "url": "/Myportfolio/static/media/postgresql.be9b5385.svg"
+  },
+  {
+    "revision": "369e2b0a4e1cf6a8e7344884f18f86ea",
+    "url": "/Myportfolio/static/media/profile.369e2b0a.webp"
+  },
+  {
+    "revision": "ef194ddf5faf63cb6cc580a4535be695",
+    "url": "/Myportfolio/static/media/react-bootstrap.ef194ddf.svg"
+  },
+  {
+    "revision": "024196716802e272adb3cbefb81fd557",
+    "url": "/Myportfolio/static/media/react-router.02419671.svg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/Myportfolio/static/media/react.4009eef3.svg"
+  },
+  {
+    "revision": "d56cc694a92ac1375822afa688760736",
+    "url": "/Myportfolio/static/media/reactnative.d56cc694.svg"
+  },
+  {
+    "revision": "8bce7aef886c3872c218e0e6ef8bdccb",
+    "url": "/Myportfolio/static/media/slide1.8bce7aef.webp"
+  },
+  {
+    "revision": "7256f309520276be70acd97720610bc6",
+    "url": "/Myportfolio/static/media/slide2.7256f309.webp"
+  },
+  {
+    "revision": "8e5b87b60151049a8df9ec0bd30978f0",
+    "url": "/Myportfolio/static/media/slide3.8e5b87b6.webp"
+  },
+  {
+    "revision": "070bf11b79c51c4543385fd172c653e4",
+    "url": "/Myportfolio/static/media/todo.070bf11b.png"
+  },
+  {
+    "revision": "3de182d2120823fe49c27eeb1765c53a",
+    "url": "/Myportfolio/static/media/typescript.3de182d2.svg"
+  }
+]);
